d626829e6327c79c17fd210a5c07895f88fd8798
Vladimir Cvetić <vladimir@ferdinand.rs>
2017-12-05 09:47:14 +0100

    Improved fromArray hydration

--- src/Entity/Traits/EntityHydrationTrait.php
+++ src/Entity/Traits/EntityHydrationTrait.php
@@ -7,17 +7,27 @@ trait EntityHydrationTrait
     /**
      * Populates entity from given array
      * @param  array  $data
-     * @return self
+     * @return mixed
      */
-    public static function fromArray(array $data) : self
+    public static function fromArray(array $data)
     {
         $class = new static();
         foreach ($data as $property => $value) {
-            if (true === is_array($value)) {
-                $value = array_pop($value);
-            }
             $method = "set{$property}";
             if (method_exists($class, $method)) {
+                $classReflection = new \ReflectionClass($class);
+                $methodReflection = $classReflection->getMethod($method);
+                $methodParams = $methodReflection->getParameters();
+                $typeHint = $methodParams[0]->getClass();
+
+                if (null !== $typeHint && true === is_array($value)) {
+                    $paramClass = $typeHint->newInstance();
+                    $paramClass = new $paramClass;
+                    $value = $paramClass::fromArray($value);
+                } elseif (true === is_array($value)) {
+                    $value = array_pop($value);
+                }
+
                 $class->$method($value);
             }
         }
